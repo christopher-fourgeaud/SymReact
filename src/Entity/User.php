d3a1f066eadb951151b7d087bf21b1de078a39a4
Christopher Fourgeaud <christopher.fourgeaud@gmail.com>
2019-07-11 01:23:20 +0200

    Installation de l'API platform et mise en place de nos ressources.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,20 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ApiResource
+ * @UniqueEntity("email", message="Un utilisateur avec cette adresse email existe déjà.")
  */
 class User implements UserInterface
 {
@@ -16,11 +23,15 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"customers_read","invoices_read", "invoices_subresource"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"customers_read","invoices_read", "invoices_subresource"})
+     * @Assert\NotBlank(message="L'adresse email est obligatoire.")
+     * @Assert\Email(message="Le format de l'adresse email doit être valide.")
      */
     private $email;
 
@@ -31,17 +42,24 @@ class User implements UserInterface
 
     /**
      * @var string The hashed password
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string")     
+     * @Assert\NotBlank(message="Le mot de passe est obligatoire.")
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read","invoices_read", "invoices_subresource"})
+     * @Assert\NotBlank(message="Le champs prénom est obligatoire.")
+     * @Assert\Length(min=3, minMessage="Le prénom doit faire entre 3 et 255 caractères", max=255, maxMessage="Le prénom doit faire entre 3 et 255 caractères.")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"customers_read","invoices_read", "invoices_subresource"})
+     * @Assert\NotBlank(message="Le champs nom de famille est obligatoire.")
+     * @Assert\Length(min=3, minMessage="Le nom de famille doit faire entre 3 et 255 caractères", max=255, maxMessage="Le nom de famille doit faire entre 3 et 255 caractères.")
      */
     private $lastName;
 
